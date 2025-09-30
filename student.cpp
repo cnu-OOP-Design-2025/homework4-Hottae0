786dcb378f729c9ea978cfa89dc530a99c6959bb
하태영 <hot_t4e0@hataeyeong-ui-MacBookAir.local>
2025-09-30 15:19:17 +0900

    과제제출

--- student.cpp
+++ student.cpp
@@ -6,29 +6,33 @@
 
 StudentRecord::StudentRecord(float midterm, float final) 
 /* TODO: Initializer list 사용해 초기화 해보기 */
+:midterm(midterm), final(final)
 {}
 
 float StudentRecord::getTotal() const {
-    /* TODO */
-    return 0.0f;
+    return midterm + final;
 }
 
 void StudentRecord::updateScores(float midterm, float final) {
+    this->midterm = midterm;
+    this->final = final;
     /* TODO: this keyword 사용해 보기 */
 }
 
 
 /* Student */
 
-Student::Student() 
-/* TODO: Initializer list 사용해 초기화 해보기 */
-{ }
+Student::Student()  
+: id(0), name(""), record(0.0f, 0.0f)  
+{}
 
 Student::Student(const char *name, int id, float midterm, float final)
 /* TODO: Initializer list 사용해 초기화 해보기 */
-{  }
+: name(name), id(id), record(midterm, final)
+{}
 
 void Student::updateRecord(const char *name, float midterm, float final) {
-    /* TODO */
+    this->name = name;
+    record.updateScores(midterm, final);
 }
 
--- student_manager.cpp
+++ student_manager.cpp
@@ -6,50 +6,127 @@ int StudentManager::last_student_id = 1000;
 
 void StudentManager::addStudent(char const* name, float midterm, float final) {
     int id = ++last_student_id;
-    /* TODO */
-
+    students[num_of_students++] = Student(name, id, midterm, final);
 }
 
 void StudentManager::deleteStudent(int id) {
     /* TODO */
+    int idx = findStudentByID(id);
+
+    if(idx != -1){
+        for(int i = idx; i < num_of_students - 1; i++){
+            students[i] = students[i + 1];
+        }
+        num_of_students--;
+    }
+
 }
 
 void StudentManager::modifyStudent(const Student& student) {
-    /* TODO */
+
+    int idx = findStudentByID(student.getID());
+
+    if(idx != -1){
+        students[idx] = student;
+    }
 }
 
 int StudentManager::findStudentByID(int id) const {
     /* TODO */
+
+    for(int i = 0; i < num_of_students; i++){
+        if(students[i].getID() == id){
+            return i;
+        }
+    }
+
     return -1;
 }
 
 int StudentManager::findBestStudentInMidterm() const {
-    /* TODO */
-    return -1;
+    float max = 0;
+    int max_student = -1;
+
+    for(int i = 0 ; i < num_of_students; i++){
+        StudentRecord sr = students[i].getRecord();
+        
+        if(sr.getMidterm() > max){
+            max = sr.getMidterm();
+            max_student = students[i].getID();
+        }
+    }
+
+    return max_student;
 }
 
 int StudentManager::findBestStudentInFinal() const {
-    /* TODO */
-    return -1;
+    float max = 0;
+    int max_student = -1;
+
+    for(int i = 0 ; i < num_of_students; i++){
+        StudentRecord sr = students[i].getRecord();
+        
+        if(sr.getFinal() > max){
+            max = sr.getFinal();
+            max_student = students[i].getID();
+        }
+    }
+
+    return max_student;
 }
 
 int StudentManager::findBestStudent() const {
-    /* TODO */
-    return -1;
+    float max = 0;
+    int max_student = -1;
+
+    for(int i = 0 ; i < num_of_students; i++){
+        StudentRecord sr = students[i].getRecord();
+        
+        if(sr.getFinal() + sr.getMidterm() > max){
+            max = sr.getFinal() + sr.getMidterm();
+            max_student = students[i].getID();
+        }
+    }
+
+    return max_student;
 }
 
 float StudentManager::getMidtermAverage() const {
-    /* TODO */
-    return 0.0f;
+    float sum = 0;
+
+    for(int i = 0 ; i < num_of_students; i++){
+        StudentRecord sr = students[i].getRecord();
+        sum += sr.getMidterm();
+    }
+
+    sum = sum / (float)num_of_students;
+
+    return sum;
 }
 
 float StudentManager::getFinalAverage() const {
-    /* TODO */
-    return 0.0f;
+    float sum = 0;
+
+    for(int i = 0 ; i < num_of_students; i++){
+        StudentRecord sr = students[i].getRecord();
+        sum += sr.getFinal();
+    }
+
+    sum = sum / (float)num_of_students;
+
+    return sum;
 }
 
 float StudentManager::getTotalAverage() const {
-    /* TODO */
-    return 0.0f;
+    float sum = 0;
+
+    for(int i = 0 ; i < num_of_students; i++){
+        StudentRecord sr = students[i].getRecord();
+        sum += sr.getFinal() + sr.getMidterm();
+    }
+
+    sum = sum / (float)num_of_students;
+
+    return sum;
 }
 
